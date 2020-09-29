ebd96fae1b28e6a9bcc99734d8a7bb646bc8e7e8
Seth Hill <sethhill28@gmail.com>
2020-09-29 08:36:02 -0700

    Descriptive Statisitcs for each variable, highlighted potential outliers, defined categorical variables

--- Group_Project_Phase_1.r
+++ Group_Project_Phase_1.r
@@ -3,9 +3,8 @@ setwd("C:/Users/sethh/OneDrive/Desktop/SFSU/Fall 2020/DS 612/Project/DS612-Data-
 data = read.csv("DS 612 Group Project data set.csv")
 
 #install packages
-library(corrplot)
-library(MASS)
-library(ISLR)
+library(corrplot) #if not previously installed run:   install.packages("corrplot")
+library(Hmisc) #if not previously installed run:   install.packages("Hmisc")
 
 #omit na values 
 data = na.omit(data)
@@ -15,29 +14,52 @@ str(data)
 
 head(data)
 data.frame(colnames(data))
-#remove unecessary columns
+#remove unnecessary columns
 data <- data[-c(1, 18, 19)]
 
-#Define qunat and qual variables
+
+#Define quant and qual variables
 quants <- subset(data, select = -date)
 quals <- data$date
 
-#Examine - what do they mean, dow we drop them? Joel
+#Outlier detection
+multi_func <- function(x){
+  c(min = min(x), mean = mean(x), max = max(x), std_dev = sd(x))
+}
+
+sapply(quants, multi_func)
+#Investigate further: price, bedrooms, yr_renovated
+
+#Define categorical variables 
+data$zipcode <- factor(data$zipcode)
+  #Potentially view, condition, and grade
+
+#Distribution of all variables 
+hist.data.frame(quants)
+
+#Examine - what do they mean, do we drop them? Joel
 #sqrft_lot(drop), sqrftlt_15(drop), sqft_living15, rate
 
-#examine relationshps between quant variables
+#examine relationships between quant variables
 cor <- round(cor(quants), 2)
 corrplot(cor, method = "number")
 
+#Explore data set add as many descriptive statistics as possible 
+aggregate(data$price ~ data$waterfront, FUN = mean)
+#visualize ^
+
+
+
 pairs(quants)
 #Determine what these results mean and starting drafting models (Tatiana, Seth and V)
-#Determine outliers
+#Determine outliers -- histrogram for distribution
 
 #drop waterfront?
-#price as dep var?
 #zip as predictor var
 
-Fi<-lm(date~ all variable, data= data)
+#Fi<-lm(date~ all variable, data= data)
+
+
 
 
 
