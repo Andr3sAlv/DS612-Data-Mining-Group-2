215e454ec72974e036a84ad3ddca27830cb28380
Seth Hill <sethhill28@gmail.com>
2020-10-02 22:24:26 -0700

    Finished cleaning, added clean_data.csv, assigned tasks 2-4

--- Group_Project_Phase_1.r
+++ Group_Project_Phase_1.r
@@ -2,6 +2,7 @@
 library(corrplot) #if not previously installed run:   install.packages("corrplot")
 library(Hmisc) #if not previously installed run:   install.packages("Hmisc")
 library("stringr") #if not previously installed run:  install.packages("stringr") 
+library(dplyr)
 
 #_______________________________________________________________________________
 #1.) Clean data
@@ -37,29 +38,36 @@ sapply(quants, multi_func)
 #Price
 hist(data$price)
 #logarithmic transformation to fix right skew in outcome variable
-data['LogPrice'] <- log(data['price'])
+data['log_price'] <- log(data['price'])
 #Insure correct transformation
-hist(data$LogPrice) #normal
+hist(data$log_price) #normal
 #Redefine quants to include LogPrice
 quants <- subset(data, select =-date)
 
 #Bedrooms
 filter(data, bedrooms == 33)
   #Only 1 house with 33 bedrooms, I think we can drop this observation
+which(data$bedroom == 33)
+data <- data[-c(15871), ]
 
 #yr_renovated
 hist(data$yr_renovated)
-#why are there so many zero values? - these are houses that hvaen't been renovated
+#why are there so many zero values? - these are houses that haven't been renovated
 t <- as.data.frame(table(data$yr_renovated))
-  #Don't need to drop, but this could be a categorical (renovated vs not renovated)
+  #Don't need to drop, but this could be a categorical (renovated vs not renovated) yes
 
 #Define categorical variables 
 data$zipcode <- factor(data$zipcode)
   #Potentially view, condition, and grade
 
-as.data.frame(table(data$view)) #potential categories: Popular: view >= 2
-as.data.frame(table(data$condition)) #category for each condition level?
-as.data.frame(table(data$grade)) #category = high if grade >= 7?
+
+as.data.frame(table(data$view)) #potential categories: Popular: view > 2 Yes
+as.data.frame(table(data$condition)) #category for each condition level? High >= 3 YEs
+as.data.frame(table(data$grade)) #category = high if grade >= 7? YEs
+
+data["popular"] <- ifelse(data$view >2, 1, 0)
+data["good_condition"] <- ifelse(data$condition >= 3, 1, 0)
+data["high_grade"] <- ifelse(data$grade >= 7, 1, 0)
 
 #Convert dates 
 str_locate(data$date, 'T') #check to make sure location is the same for each row 
@@ -73,26 +81,63 @@ hist.data.frame(quants)
 cor <- round(cor(quants), 2)
 corrplot(cor, method = "number")
 
-#Examine - what do they mean, do we drop them? Joel
-#sqrft_lot(drop), sqrftlt_15(drop), sqft_living15
+pairs.panels(quants) #Determine any remaining transformations of variables: none 
 
-pairs.panels(quants) #Determine any remaining transformations of variables 
-
-#_______________________________________________________________________________
-#2.) Descriptive Statistics
-#write code here 
-
-
+#export to csv save as clean_data
+write.csv(data, file = "clean_data.csv", col.names = TRUE)
 
 #_______________________________________________________________________________
-#3.) Model Regression problem
-#write code here
+#2.) Descriptive Statistics: JOel and V
+#Write code in seperate file, make pull request to upload to repo
+#Feel free to use these to start
+#These plots focus on the variables with highest correlation
+clean_data <- read.csv(clean_data.csv)
+
+#summary
+summary(clean_data)
+
+#correlation matrix of cleaned data
+clean_quants <- subset(clean_data, select = -date)
+cor <- round(cor(), 2)
+corrplot(cor, method = "number", title = "Correlation Matrix of Quantitative Variables")
+
+#Average price per bathroom class 
+a <- aggregate(clean_data$price ~ data$bathroom, FUN = mean)
+names(a)[1] = "Bathrooms"
+names(a)[2] = "Avg Price"
+a
+
+#Another aggregation?
+#Average price per grade 
+b <- aggregate(clean_data$price ~ data$grade, FUN = mean)
+names(b)[1] = "Grade"
+names(b)[2] = "Avg Price"
+b
+
+#Some scatter plots 
+#Sqft_living vs price 
+plot(clean_data$sqft_living, clean_data$price)
+
+#Prices through time 
+plot(clean_data$date, clean_data$price)
+
+#Histogram 
+
+#Box plot
+
+
+#_______________________________________________________________________________
+#3.) Model Regression problem: Jimmy and Tatiana
+#make sure to use log(price) not price
+#write code in separate file, make pull request to upload to repo
+#Correlation matrix might be helpful if using forward selection
 
 
 
 #_______________________________________________________________________________
-#4.) Model classification problem
-#write code here
+#4.) Model classification problem Seth, etc. : 
+#write in seperate file, make pull request to upload to repo
 
+#R-markdown: V, Tatina, and anyone else 
 
 
